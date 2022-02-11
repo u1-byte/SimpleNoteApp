2559172b6027f00cad14704d44bbd3accf6e39ae
Yuma Gunawan <amazingyuma0@gmail.com>
2022-02-11 12:57:08 +0800

    tidy up template

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,16 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
     return render_template("login.html", boolean=True)
 
 @auth.route('/logout')
 def logout():
-    return "<p>Logout</p>"
+    return render_template("logout.html")
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    return "<p>Sign Up</p>"
+    return render_template("sign_up.html")
